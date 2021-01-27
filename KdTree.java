dc479f5da8752c1723fd5d392234a78c0a8aa15c
AJ San Joaquin <ajsanjoaquin@gmail.com>
2021-01-27 09:30:37 +0800

    fix kdtree

--- KdTree.java
+++ KdTree.java
@@ -5,16 +5,15 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 
+import edu.princeton.cs.algs4.StdOut;
 
 
 public class KdTree {
     private int treeSize = 0;
-    private Point2D champion;
     private Node root;
-    private double xMin, xMax, yMin, yMax;
+    private Point2D champion;
     // construct an empty set of points
     public KdTree() {
     } 
@@ -42,12 +41,15 @@ public class KdTree {
     // add the point to the set (if it is not already in the set)                          
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
+        // if the point is in a unit square
         if( (p.x() >=0 && p.x() <=1) &&
         (p.y() >=0 && p.y() <=1) ) {
             // check if unique first
-            if (!contains(p)) treeSize++;
-            // vertical = false, since we start with horizontal comparison
-            root = put(root, p, false);
+            if (!contains(p)) {
+                treeSize++;
+                // vertical = false, since we start with horizontal comparison
+                root = put(root, p, false);
+            }
             
         }
     }
@@ -55,9 +57,15 @@ public class KdTree {
     private Node put(Node x, Point2D key, boolean orientation) {
         if (x == null) {
             Node base = new Node(key);
-            base.rect = new RectHV(0 , 0 ,1 ,1);
+            base.rect = new RectHV(0, 0, 1, 1);
             return base;
         }
+
+        double xMax = x.rect.xmax();
+        double xMin = x.rect.xmin();
+        double yMax = x.rect.ymax();
+        double yMin = x.rect.ymin();
+
         // horizontal case
         if (!orientation){
             int cmp = Double.compare(key.x(), x.p.x());
@@ -65,18 +73,12 @@ public class KdTree {
                 x.lb  = put(x.lb,  key, true);
                 // x.lb is already VERTICAL
                 xMax = x.p.x();
-                xMin = x.rect.xmin();
-                yMax = x.rect.ymax();
-                yMin = x.rect.ymin();
                 x.lb.rect = new RectHV(xMin, yMin, xMax, yMax);
             }
             else {
                 x.rt = put(x.rt, key, true);
                 // x.rt is already VERTICAL
                 xMin = x.p.x();
-                xMax = x.rect.xmax();
-                yMax = x.rect.ymax();
-                yMin = x.rect.ymin();
                 x.rt.rect = new RectHV(xMin, yMin, xMax, yMax);
             }
         }
@@ -87,18 +89,12 @@ public class KdTree {
                 x.lb  = put(x.lb,  key, false);
                 // x.lb is already HORIZONTAL
                 yMax = x.p.y();
-                yMin = x.rect.ymin();
-                xMin = x.rect.xmin();
-                xMax = x.rect.xmax();
                 x.lb.rect = new RectHV(xMin, yMin, xMax, yMax);
             }
             else {
                 x.rt = put(x.rt, key, false);
                 // x.rt is already HORIZONTAL
                 yMin = x.p.y();
-                yMax = x.rect.ymax();
-                xMin = x.rect.xmin();
-                xMax = x.rect.xmax();
                 x.rt.rect = new RectHV(xMin, yMin, xMax, yMax);
             }
         }
@@ -122,6 +118,7 @@ public class KdTree {
             if      (cmp < 0) return get(x.lb, key, true);
             else if (cmp >= 0 && !x.p.equals(key)) return get(x.rt, key, true);
         }
+        // vertical case
         else{
             int cmp = Double.compare(key.y(), x.p.y());
             if      (cmp < 0) return get(x.lb, key, false);
@@ -177,33 +174,51 @@ public class KdTree {
     private void traverse(Node x, RectHV rect, Stack<Point2D> solList) {
         if (x == null) return;
         // pruning rule; can be optimized further if you just compare with splitting line
-        if (!x.rect.intersects(rect)) return;
+        if (!rect.intersects(x.rect)) return;
+        if (rect.contains(x.p)) solList.push(x.p);
         traverse(x.lb, rect, solList);
         traverse(x.rt, rect, solList);
-        if (rect.contains(x.p)) solList.push(x.p);
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty 
     public Point2D nearest(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
         if (isEmpty()) return null;
-        champion = new Point2D(2,2);
-        traverseNearest(root, p);
+        champion = root.p;
+        traverseNearest(root, p, false);
         return champion;
     }
 
-    private void traverseNearest(Node x, Point2D p) {
+    private void traverseNearest(Node x, Point2D p, boolean orientation) {
         if (x == null) return;
-        if (x.rect.distanceSquaredTo(p) > champion.distanceSquaredTo(p)) return;
-        traverseNearest(x.lb, p);
-        traverseNearest(x.rt, p);
-        if (x.p.distanceSquaredTo(p) < champion.distanceSquaredTo(p)) champion = x.p;
+
+        double minDist = champion.distanceSquaredTo(p);
+        double selectedDist = x.p.distanceSquaredTo(p);
+        // pruning rule: do not search the subtrees of the node whose rectangle distance is farther than min
+        if (x.rect.distanceSquaredTo(p) >= minDist) return;
+        if (selectedDist < minDist) champion = x.p;
+
+        // Optimization: search the points on the same side of splitting line of query point first 
+        boolean coordsLess;
+        // horizontal case
+        if (!orientation) { coordsLess = p.x() < x.p.x(); }
+        else { coordsLess = p.y() < x.p.y(); }
+        
+        if (coordsLess) {
+            // if query point is less than selected point from Tree, search left/bottom first
+            traverseNearest(x.lb, p, true);
+            traverseNearest(x.rt, p, true);
+        }
+        else {
+            traverseNearest(x.rt, p, true);
+            traverseNearest(x.lb, p, true);
+        }
     }
     // unit testing of the methods (optional) 
     public static void main(String[] args) {
         KdTree test = new KdTree();
         In in = new In(args[0]);
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 10; i++) {
             double x = in.readDouble();
             double y = in.readDouble();
             Point2D point = new Point2D(x, y);
@@ -214,7 +229,7 @@ public class KdTree {
 
         //RectHV testing = new RectHV(0.5, 0.5, 1, 1);
         //for (Point2D point : test.range(testing))
-        //StdOut.println(point);
+        StdOut.println(test.range(new RectHV(0.875, 0.625, 1, 0.875)));
 
        /* Point2D a = new Point2D(.7, .2);
         Point2D b = new Point2D(.5, .4);
